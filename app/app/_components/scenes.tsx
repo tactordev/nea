14a3dcf32429b37f6e84d62c3f3e7804aff5f252
Tactor <119103845+tactordev@users.noreply.github.com>
2026-08-27 10:22:40 +0100

    [APP/Scenes] Remove title function in favour of global /lib/formatters utility

--- app/app/_components/scenes.tsx
+++ app/app/_components/scenes.tsx
@@ -8,12 +8,6 @@ interface FilterProps extends React.HTMLAttributes<HTMLDivElement> {
     width?: string;
 }
 
-function title(value: string) {
-    return value.split(" ").map((v) => {
-        return `${v.slice(0, 1).toUpperCase()}${v.length > 1 ? v.slice(1) : ""}`;
-    }).join(" ");
-}
-
 function Filter(
     {
         name,
