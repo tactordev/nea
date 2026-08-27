5339107b33491684592622c7cce8449d957bcc5e
Tactor <119103845+tactordev@users.noreply.github.com>
2026-08-27 11:58:23 +0100

    [APP/Map] Map viewer

--- app/app/_components/map.tsx
+++ app/app/_components/map.tsx
@@ -1,6 +1,11 @@
+import LF from "./lf";
 import Section from "./section";
 
 
 export default function Map() {
-    return <Section title="Map" />
+    return (
+        <Section title="Map">
+            <LF posix={[51.505, -0.09]} zoom={10} />
+        </Section>
+    );
 }
