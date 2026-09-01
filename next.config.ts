d11bdf3af1e718c86b92591f7e9c53ff53fbe9d7
Tactor <119103845+tactordev@users.noreply.github.com>
2026-09-01 15:04:09 +0100

    [GLOBAL] Dynamically allow local IPs for dev origins

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
+import os from "os";
+
+function getIps() {
+  const interfaces = os.networkInterfaces();
+  const ips = [];
+
+  for (const name of Object.keys(interfaces)) {
+    for (const net of interfaces[name]!) {
+      if (net.family === "IPv4" && !net.internal) {
+        ips.push(net.address);
+      }
+    }
+  }
+
+  return ips;
+}
+
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: getIps(),
 };
 
 export default nextConfig;
