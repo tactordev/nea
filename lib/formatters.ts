6937a85e8b49e927e099476fa4ef8931166d37aa
Tactor <119103845+tactordev@users.noreply.github.com>
2026-09-01 16:18:40 +0100

    [GLOBAL/QOL] Title formatter can parse camelCase variable names.

--- lib/formatters.ts
+++ lib/formatters.ts
@@ -1,7 +1,34 @@
 
 
-export function title(value: string): string {
-    return value.split(" ").map((v) => {
+function split(value: string, detector: string): string[] {
+    switch (detector) {
+        case "capital":
+            let count = 0;
+            let latestDeletion = 0;
+            const chunks: string[] = [];
+            for (const char of value) {
+                console.log(char);
+                if (char.toLowerCase() !== char) {
+                    console.log("found capital");
+                    const chunk = value.slice(latestDeletion, count);
+                    console.log("chunk", chunk);
+                    chunks.push(chunk);
+                    latestDeletion = count;
+                }
+                count++;
+            }
+
+            const finalChunk = value.slice(latestDeletion);
+            chunks.push(finalChunk);
+
+            return chunks;
+        default:
+            return value.split(detector);
+    }
+}
+
+export function title(value: string, type?: string): string {
+    return split(value, type === "camel" ? "capital" : " ").map((v) => {
         return `${v.slice(0, 1).toUpperCase()}${v.length > 1 ? v.slice(1) : ""}`;
     }).join(" ");
 };
